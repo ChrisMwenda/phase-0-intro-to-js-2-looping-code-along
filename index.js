f9af071c228bded8bdfe1280835a375967263f3e
Christopher <chrismbogo97@gmail.com>
2022-08-30 08:06:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(obj,event){
+   let message=[]
+   for(let i=0;i<obj.length;i++){
+   message.push(`Thank you, ${obj[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+writeCards(['Guadalupe','Ollie', 'Aki'],"surprise");
+
+function countDown(num1){
+    while(num1>=0){
+        console.log(num1--);     
+
+    }
+}
+countDown(4);
